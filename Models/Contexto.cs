13926295c88bdfc73c45afe28bb4a67cade6ae0c
Felipe Hidequel <felipehidequel@gmail.com>
2023-10-28 20:54:02 -0300

    criação de contexto para classe Cliente

---
+++ Models/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using ClientesSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clientes.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        { 
+            
+        }
+
+        public DbSet<Cliente> Cliente { get; set; }
+    }
+}
